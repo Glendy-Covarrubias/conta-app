215bddc9f423bed026e2857e3180a7d4738cb918
Glendy-Covarrubias <glendycovarrubias@hotmail.com>
2025-03-19 16:53:09 -0600

    Mejoras
    -Configuración de algunos archivos para el copilado o minificación del Deploy
    -Mejoras para reducir código repetido
    -Sacando el zip de los archivos para el index

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined
 };
 
 export default nextConfig;
